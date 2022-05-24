b4157778f0a10178fda6994a84fe672e074ac0a8
AliaaIbrahemAhmed <aliaa.ebrahem21@gmail.com>
2022-05-24 03:19:12 +0200

    checkers modification

--- src/Turns/CheckersTurn.scala
+++ src/Turns/CheckersTurn.scala
@@ -1,4 +1,4 @@
 package Turns
 
-class CheckersTurn(var newIndex: Array[Int],var prevIndex: Array[Int], var player : Boolean, var isValid : Boolean) {
+class CheckersTurn(var newIndex: Array[Int],var prevIndex: Array[Int], var isValid : Boolean) {
 }
